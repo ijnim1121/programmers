0425a31ad70ff707a53bb8a98b41090d8a7818fb
ijnim1121 <jimin3347@naver.com>
2024-05-23 13:12:35 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,13 @@
+import math
+
+def solution(price):
+    if price >= 100000 and price <300000:
+        answer = price - (price*0.05)
+    elif price >= 300000 and price < 500000:
+        answer = price - (price*0.1)
+    elif price >= 500000:
+        answer = price - (price*0.2)
+    else: 
+        answer = price
+    answer_int = math.floor(answer)
+    return answer_int
